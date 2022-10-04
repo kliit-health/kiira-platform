fc0aa8be3689c8380cfbcb22197a81bb4719aa1d
Manuel Silva <msilva28.dev@gmail.com>
2022-10-03 22:13:09 -0700

    Fixed duplicate header error

--- firebase/functions/src/checkAppointmentCost/index.func.ts
+++ firebase/functions/src/checkAppointmentCost/index.func.ts
@@ -1,8 +1,8 @@
 import {Context} from "../ioc";
-import {servicePricing} from "../core/bll/services/service-pricing";
+import {PricingInfo, servicePricing} from "../core/bll/services/service-pricing";
 import {firestoreUserLookup} from "../db/firestore-user-lookup";
 import {firestoreServiceLookup} from "../db/firestore-service-lookup";
-import {Maybe, MaybeAsync, NonEmptyList} from "purify-ts";
+import {EitherAsync, Maybe, MaybeAsync, NonEmptyList} from "purify-ts";
 import * as admin from "firebase-admin";
 import {auth} from "firebase-admin";
 import DecodedIdToken = auth.DecodedIdToken;
@@ -11,42 +11,36 @@ module.exports = (context: Context) => {
   const userLookup = firestoreUserLookup(context.firestore);
   const serviceLookup = firestoreServiceLookup(context.firestore);
   return context.functions.https.onRequest(async (req, res) => {
+    context.logger.info("request body", req.body);
     const idToken: DecodedIdToken | undefined = (await decodeToken(req.headers.authorization)).extract();
     if (!idToken) {
       res.sendStatus(401);
       return;
     }
 
-    const result = await MaybeAsync(async ({fromPromise, liftMaybe}) => {
-      const appointmentTypeId = await liftMaybe(
-        Maybe.fromNullable(req.body.appointmentTypeId)
-          .ifNothing(() => badRequest("No appointment id provided")),
+    const result = await EitherAsync<string, PricingInfo>(async ({fromPromise, liftEither}) => {
+      const appointmentTypeId = await liftEither(
+        Maybe.fromNullable(req.body.appointmentTypeId).toEither("No appointment id provided"),
       );
       const credits = await fromPromise(
-        userLookup.credits(idToken.uid)
-          .ifNothing(() => badRequest("Couldn't fetch user")),
+        userLookup.credits(idToken.uid).toEitherAsync("Couldn't fetch user"),
       );
       const cost = await fromPromise(
-        serviceLookup.cost(appointmentTypeId)
-          .ifNothing(() => badRequest("Couldn't fetch appointment type")),
+        serviceLookup.cost(appointmentTypeId).toEitherAsync("Couldn't fetch appointment type"),
       );
       return servicePricing(credits, cost);
     });
 
     result.caseOf({
-      Just: value => {
+      Right: value => {
+        context.logger.info("success", value);
         res.status(200).send(value);
       },
-      Nothing: () => badRequest(),
+      Left: (errorMessage: string) => badRequest(errorMessage),
     });
 
-    function badRequest(message?: string) {
-      if (message != null) {
-        context.logger.info(message);
-        res.status(400).send({error: message});
-        return;
-      }
-      res.sendStatus(400);
+    function badRequest(message: string) {
+      res.status(400).send({error: message});
     }
   });
 };
